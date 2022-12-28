a9c92284d3ea6f835a7312af0b0f5a9d114c222d
Pranauv Kumar <pranauv1803@gmail.com>
2022-12-28 16:06:50 +0530

    added mail options

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import {getAllProjects,getSelectProject} from '../controllers/userController';
+import {getAllProjects,getSelectProject, sendMailAfterContact} from '../controllers/userController';
 const router = express.Router();
 
 router.get('/',getAllProjects);
 router.get('/:pname',getSelectProject);
+router.post('/contact',sendMailAfterContact);
 
 export default router;
